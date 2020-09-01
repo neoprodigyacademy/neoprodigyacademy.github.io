d8c409145097afc0e4257aac4b8f56174f9e1077
itsraxeira <itsraxeira@gmail.com>
2020-09-01 11:35:19 +0700

    Add civitas endpoint

---
+++ civitas/script.js
@@ -0,0 +1,113 @@
+const faceclaim = document.querySelector("#faceclaim");
+const clanname = document.querySelector("#clanname");
+const yourname = document.querySelector("#yourname");
+const prodigium = document.querySelector("#prodigium");
+const status = document.querySelector("#status");
+const registerbutton = document.querySelector("#register-button");
+const searchBar = document.querySelector("#search-bar");
+
+var user;
+
+function register() {
+    if (faceclaim.value && clanname.value && yourname.value && prodigium.value && status.value) {
+        var biodata = {
+            faceClaim: faceclaim.value,
+            clanName: clanname.value,
+            name: yourname.value,
+            prodigy: prodigium.value,
+            statusOfFaceClaim: status.value,
+        };
+
+        user = biodata;
+
+        db.collection("NPA-DB")
+            .add(biodata)
+            .then((docRef) => {
+                console.log("Document written with ID: ", docRef.id);
+                alert("Your data is successfully registered!");
+                faceclaim.value = "";
+                clanname.value = "";
+                yourname.value = "";
+                prodigium.value = "";
+                status.value = "";
+
+                location.reload();
+            })
+            .catch((error) => {
+                console.error("Error adding document: ", error);
+                alert(error);
+            })
+    } else {
+        alert("All fields should be filled!");
+    }
+}
+
+registerbutton.addEventListener("click", register);
+
+var table;
+
+function prepareTable(dataset) {
+    table = $('#table_id').DataTable({
+        paging: true,
+        data: dataset,
+        dom: '<"top"i>rt<"bottom"><"clear">',
+        columns: [
+            { data: 'faceClaim' },
+            { data: 'clanName' },
+            { data: 'name' },
+            { data: 'prodigy' },
+            { data: 'statusOfFaceClaim' }
+        ]
+    });
+
+
+    // setup search
+    searchBar.addEventListener("keyup", function(event) {
+        table.search(searchBar.value).draw();
+        console.log(searchBar.value);
+    });
+}
+
+var queryResult = [];
+
+function loadDatabase() {
+    db.collection("NPA-DB").get()
+        .then(function(querySnapshot) {
+            // on success, create dataset list
+            querySnapshot.forEach(function(doc) {
+                let student = doc.data();
+                queryResult.push({
+                    faceClaim: student.faceClaim,
+                    clanName: student.clanName,
+                    name: student.name,
+                    prodigy: student.prodigy,
+                    statusOfFaceClaim: student.statusOfFaceClaim,
+                });
+            });
+
+            // prepare data table
+            prepareTable(queryResult);
+        });
+}
+
+function refreshDataset() {
+    db.collection("NPA-DB").get()
+    .then(function(querySnapshot) {
+        // on success, create dataset list
+        querySnapshot.forEach(function(doc) {
+            let student = doc.data();
+            queryResult.push({
+                faceClaim: student.faceClaim,
+                clanName: student.clanName,
+                name: student.name,
+                prodigy: student.prodigy,
+                statusOfFaceClaim: student.statusOfFaceClaim,
+            });
+        });
+
+        // prepare data table
+        table.data(queryResult).draw(false);
+    });
+}
+
+window.onload = loadDatabase;
---
+++ scripts/firebase.js
@@ -0,0 +1,13 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyC8JH_xK-n3RPChWBWyoGrzD7N3vmQ7YbU",
+    authDomain: "flameseeker-56fb5.firebaseapp.com",
+    databaseURL: "https://flameseeker-56fb5.firebaseio.com",
+    projectId: "flameseeker-56fb5",
+    storageBucket: "flameseeker-56fb5.appspot.com",
+    messagingSenderId: "121065227260",
+    appId: "1:121065227260:web:02a46a9c0cbbfc86656879"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+var db = firebase.firestore();
