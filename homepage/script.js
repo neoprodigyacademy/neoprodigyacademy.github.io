7daca8f4a19ec7f62071654581fb8b472a1c08c6
Rido Ramadan <rido.ramadan@gmail.com>
2020-11-17 22:59:22 +0700

    Add sidebar

--- homepage/script.js
+++ homepage/script.js
@@ -1,6 +1,12 @@
-/*
-INSPIRED BY:
-https://dribbble.com/shots/11012652--Timed-Cards-Opening
-*/
-
 const registerButton = document.querySelector("#register_btn");
+
+const form = document.querySelector(".form");
+const closeBtn = document.querySelector("#hideRegister");
+closeBtn.onclick = () => {
+    form.style.left = "-540px";
+}
+
+const registerBtn = document.querySelector("#register_btn");
+registerBtn.onclick = () => {
+    form.style.left = "0";
+}
