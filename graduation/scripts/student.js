72a26b13f97826a41d4edc87763b7e3a22c3a859
itsraxeira <itsraxeira@gmail.com>
2020-12-05 22:04:16 +0700

    Edit score

--- graduation/scripts/student.js
+++ graduation/scripts/student.js
@@ -65,4 +65,4 @@ class Student {
 
 var student = null;
 
-[["A","AB","AB","AB","AB","AB","B"], ["AB","AB","AB","AB","AB","AB","AB"],["A","A","A","AB","AB","B","B"],["A","A","A","A","B","B","B"],["A","A","AB","AB","AB","AB","B"],["A","AB","AB","AB","AB","AB","AB"],["A","A","A","A","AB","B","B"],["A","A","AB","AB","AB","AB","AB"],["A","A","A","AB","AB","AB","B"],["A","A","A","A","A","B","B"],["A","A","A","AB","AB","AB","AB"],["A","A","A","A","AB","AB","B"], ["A","A","A","A","AB","AB","AB"],["A","A","A","A","A","AB","B"]] 
+[["A","AB","AB","AB","AB","B","AB"], ["AB","AB","AB","AB","AB","AB","AB"],["A","A","A","AB","AB","B","B"],["A","A","A","A","B","B","B"],["A","A","AB","AB","AB","B","AB"],["A","AB","AB","AB","AB","AB","AB"],["A","A","A","A","AB","B","B"],["A","A","AB","AB","AB","AB","AB"],["A","A","A","AB","AB","B","AB"],["A","A","A","A","A","B","B"],["A","A","A","AB","AB","AB","AB"],["A","A","A","A","AB","B","AB"], ["A","A","A","A","AB","AB","AB"],["A","A","A","A","A","B","AB"]] 
