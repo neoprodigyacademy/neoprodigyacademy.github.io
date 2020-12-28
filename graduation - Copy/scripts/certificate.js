24e7d3b8b47bf82fb084812fb528ef2c3593fb8e
itsraxeira <itsraxeira@gmail.com>
2020-12-28 14:50:10 +0700

    Adding asstes

---
+++ graduation - Copy/scripts/certificate.js
@@ -0,0 +1,27 @@
+const IMG_CERTIFICATE = "../assets/certificate_";
+const CERT_WIDTH = 1000;
+const CERT_HEIGHT = 1425;
+const FONT_SIZE = 48;
+const NAME_HEIGHT_OFFSET = 430;
+
+const PP_WIDTH = 877 - 736;
+const PP_HEIGHT = 465 - 276;
+
+function drawCertificate(url, name, canvas) {
+    let c2D = canvas.getContext("2d");
+    canvas.width = CERT_WIDTH;
+    canvas.height = CERT_HEIGHT;
+
+    const image = new Image();
+    image.src = url;
+    image.onload = () => {
+        c2D.drawImage(image, 0, 0, canvas.width, canvas.height);
+        drawName(name, c2D, "Times New Roman");
+    }
+}
+
+function drawName(name, context, font = "Times New Roman") {
+    context.textAlign = "center";
+    context.font = "bold " + FONT_SIZE + "px " + font;
+    context.fillText(name, CERT_WIDTH / 2, NAME_HEIGHT_OFFSET);
+}
